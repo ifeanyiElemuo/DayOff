4061fde0abdb423e32a0ec4c9743f0d4e3138eba
Darfy <darfy009@gmail.com>
2023-10-14 12:57:44 +0100

    --  added CRUD functions

---
+++ DayOff.Web/Configurations/MapperConfig.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using DayOff.Web.Data;
+using DayOff.Web.Models;
+
+namespace DayOff.Web.Configurations
+{
+    public class MapperConfig : Profile
+    {
+        public MapperConfig()
+        {
+            CreateMap<LeaveType, LeaveTypeVM>().ReverseMap();
+        }
+    }
+}
---
+++ DayOff.Web/Models/LeaveTypeVM.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DayOff.Web.Models
+{
+    public class LeaveTypeVM
+    {
+        public int Id { get; set; }
+        [Display(Name = "Leave Name")]
+        public required string Name { get; set; }
+
+        [Display(Name = "Default Number of Days")]
+        [Range(1, 25, ErrorMessage = "Please enter a valid number")]
+        public required int DefaultDays { get; set; }
+    }
+}
